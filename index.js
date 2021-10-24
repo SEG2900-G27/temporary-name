b385517a5525d6b137589473f12fb75cf2fc0c8f
Noah do Rego <ndore051@uottawa.ca>
2021-10-24 16:58:07 -0400

    Added logos & implemented smooth scrolling with JS through the navbar

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+const navlinks = document.querySelectorAll(".navbar ul a");
+ 
+for (const link of navlinks) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
