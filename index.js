96a1f259e059710e4af8da674c69d052fe5dd6fb
Andrew Pham <andrewbapham@outlook.com>
2021-11-09 19:35:04 -0500

    Changed intersection observer margin to show "unloading"/slide out of text boxes in "Who are we"

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function clickHandler(e) {
 
 const options = {
   threshold: 0,
-  rootMargin: '-10% 0px -10% 0%'
+  rootMargin: ' -30% 0px -10% 0%'
 };
 
 const observer = new IntersectionObserver(function(entries, observer){
